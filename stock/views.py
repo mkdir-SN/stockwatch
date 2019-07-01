89cd0eaead0350fd0c63da37fa1b2c015ec76e7b
Shirley Nguyen <Shirley@Shirleys-MacBook-Pro-3.local>
2019-07-01 10:44:46 -0700

    Added comments and cleaned up code

--- stock/views.py
+++ stock/views.py
@@ -9,7 +9,7 @@ def addticker_view(request):
 	form = TickerForm(request.POST, user=request.user)
 	if form.is_valid():
 		instance = form.save(commit=False)
-		instance.user = request.user
+		instance.user = request.user 
 		instance.save()
 		return redirect('/watchlist')
 	context = {'form': form}
@@ -19,12 +19,13 @@ def addticker_view(request):
 def watchlist_view(request):
 	qs = Ticker.objects.filter(user=request.user)
 	username = request.user.username
-	# access model's Stock object for each qs object and pass into template as context variable
-	stock_objs = [(Stock(function=q.time_series, symbol=q.ticker, interval=q.interval), q.pk) for q in qs[:6]]
+	stock_objs = [(Stock(function=q.time_series, symbol=q.ticker, interval=q.interval), q.pk) for q in qs]
+	# Create an in-depth list of stock names and prices to access within template by index
 	stock_name_and_prices = [(s.stock_symbol(), s.stock_info()[2], s.stock_times(), s.stock_prices(), pk) for s, pk in stock_objs]
+	# Index: 1. stock symbol (string), 2. stock information (string), 3. stock times (list), 4. stock prices (dictionary), 5. ticker ID in DB (int)
 	if request.method == "POST":
-	    # Fetch list of items to delete, by pk
-	    items_to_delete = request.POST.getlist('delete') # value of input 
+		# Handles deletion of rows in Ticker model based on user selection in template
+	    items_to_delete = request.POST.getlist('delete') 
 	    Ticker.objects.filter(pk__in=items_to_delete).delete()
 	    return redirect('/watchlist')
 	context = {'stock_name_and_prices': stock_name_and_prices, 'username': username}
